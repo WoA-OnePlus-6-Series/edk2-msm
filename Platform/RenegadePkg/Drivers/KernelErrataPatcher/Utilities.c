be56ebaff9688bbc41936c8d8c205a346ff2197a
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-10-05 15:58:40 +0200

    Update Utilities.c

--- Platform/RenegadePkg/Drivers/KernelErrataPatcher/Utilities.c
+++ Platform/RenegadePkg/Drivers/KernelErrataPatcher/Utilities.c
@@ -14,57 +14,87 @@
 **/
 #include "KernelErrataPatcher.h"
 
-VOID CopyMemory(
-    EFI_PHYSICAL_ADDRESS destination, EFI_PHYSICAL_ADDRESS source, UINTN size)
+#define IMAGE_DOS_SIGNATURE 0x5A4D
+#define IMAGE_NT_SIGNATURE  0x00004550
+
+EFI_PHYSICAL_ADDRESS
+LocateWinloadBase(
+  EFI_PHYSICAL_ADDRESS base,
+  UINTN               *size)
+{
+  if (base & (EFI_PAGE_SIZE - 1)) {
+    base &= ~(EFI_PAGE_SIZE - 1);
+    base += EFI_PAGE_SIZE;
+  }
+
+  do {
+    if (*(UINT16 *)base == IMAGE_DOS_SIGNATURE) {
+      UINT32               newBaseOffset = *(UINT32 *)(base + 0x3C);
+      EFI_PHYSICAL_ADDRESS newBase       = base + newBaseOffset;
+
+      if (*(UINT16 *)newBase == IMAGE_NT_SIGNATURE) {
+        *size = *(UINT32 *)(newBase + 0x110);
+
+        if (*size & (EFI_PAGE_SIZE - 1)) {
+          *size &= ~(EFI_PAGE_SIZE - 1);
+          *size += EFI_PAGE_SIZE;
+        }
+
+        break;
+      }
+    }
+
+    base -= EFI_PAGE_SIZE;
+  } while (TRUE);
+
+  return base;
+}
+
+VOID
+CopyMemory(
+  EFI_PHYSICAL_ADDRESS destination,
+  EFI_PHYSICAL_ADDRESS source,
+  UINTN                size)
 {
   UINT8 *dst = (UINT8 *)(destination);
   UINT8 *src = (UINT8 *)(source);
+
   for (UINTN i = 0; i < size; i++) {
     dst[i] = src[i];
   }
 }
 
 EFI_PHYSICAL_ADDRESS
-FindPattern(EFI_PHYSICAL_ADDRESS baseAddress, UINTN size, const CHAR8 *pattern)
+FindPattern(
+  EFI_PHYSICAL_ADDRESS baseAddress,
+  UINTN                size,
+  CONST CHAR8         *pattern)
 {
   EFI_PHYSICAL_ADDRESS firstMatch     = 0;
-  const CHAR8         *currentPattern = pattern;
+  CONST CHAR8         *currentPattern = pattern;
 
-  for (EFI_PHYSICAL_ADDRESS current = baseAddress; current < baseAddress + size;
-       current++) {
+  for (EFI_PHYSICAL_ADDRESS current = baseAddress; current < baseAddress + size; current++) {
     UINT8 byte = currentPattern[0];
-    if (!byte)
+
+    if (!byte) {
       return firstMatch;
-    if (byte == '\?' ||
-        *(UINT8 *)(current) == GET_BYTE(byte, currentPattern[1])) {
-      if (!firstMatch)
-        firstMatch = current;
-      if (!currentPattern[2])
-        return firstMatch;
-      ((byte == '\?') ? (currentPattern += 2) : (currentPattern += 3));
     }
-    else {
+      
+    if (byte == '\?' || *(UINT8 *)(current) == GET_BYTE(byte, currentPattern[1])) {
+      if (!firstMatch) {
+        firstMatch = current;
+      }
+        
+      if (!currentPattern[2]) {
+        return firstMatch;
+      }
+
+      ((byte == '\?') ? (currentPattern += 2) : (currentPattern += 3));
+    } else {
       currentPattern = pattern;
       firstMatch     = 0;
     }
   }
 
   return 0;
-}
-
-KLDR_DATA_TABLE_ENTRY *GetModule(LIST_ENTRY *list, const CHAR16 *name)
-{
-  for (LIST_ENTRY *entry = list->ForwardLink; entry != list;
-       entry             = entry->ForwardLink) {
-
-    PKLDR_DATA_TABLE_ENTRY module =
-        CONTAINING_RECORD(entry, KLDR_DATA_TABLE_ENTRY, InLoadOrderLinks);
-
-    if (module &&
-        StrnCmp(name, module->BaseDllName.Buffer, module->BaseDllName.Length) ==
-            0)
-      return module;
-  }
-
-  return NULL;
 }
